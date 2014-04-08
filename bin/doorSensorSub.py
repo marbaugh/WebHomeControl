9f148b2578504d82fd41dd6e4d0174ffd1f79bf6
marbaugh <marbaugh@gmail.com>
2014-04-07 21:56:09 -0400

    Added init and bin folders
    Added init files for subscribing and scripts for subscribing  to the Pi
    publishers

---
+++ bin/doorSensorSub.py
@@ -0,0 +1,22 @@
+
+import sys
+import time
+import zmq
+
+port = "5556"
+# Socket to talk to server
+context = zmq.Context()
+socket = context.socket(zmq.SUB)
+
+def sub_door_sensor_status():
+print "Collecting updates from weather server..."
+socket.connect ("tcp://localhost:%s" % port)
+topicfilter = "10001"
+while True:
+socket.setsockopt(zmq.SUBSCRIBE, topicfilter)
+string = socket.recv()
+topic, messagedata = string.split()
+print topic, messagedata
+
+if __name__ == "__main__":
+	sub_motion_sensor_status()
---
+++ bin/motionSensorSub.py
@@ -0,0 +1,21 @@
+import sys
+import time
+import zmq
+
+port = "5556"
+# Socket to talk to server
+context = zmq.Context()
+socket = context.socket(zmq.SUB)
+
+def sub_motion_sensor_status():
+print "Collecting updates from weather server..."
+socket.connect ("tcp://localhost:%s" % port)
+topicfilter = "10000"
+while True:
+socket.setsockopt(zmq.SUBSCRIBE, topicfilter)
+string = socket.recv()
+topic, messagedata = string.split()
+print topic, messagedata
+
+if __name__ == "__main__":
+	sub_motion_sensor_status()
