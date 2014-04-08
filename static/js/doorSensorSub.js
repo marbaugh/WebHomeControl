d2fb027e7e6e1ed8dcaeb5463ff0bb9947bd831b
unknown <matthew.arbaugh@LNDLE641GNXJDP1.disanet.disa-u.mil>
2014-04-08 10:59:17 -0400

    Adding javascript subscribers for motion and door sensors

---
+++ static/js/doorSensorSub.js
@@ -0,0 +1,9 @@
+var zeromq = require('zmq');
+ 
+var sock = zeromq.socket('sub');
+sock.connect('tcp://127.0.0.1:5556');
+sock.subscribe('door');
+ 
+sock.on('message', function(data) {
+   console.log(data.toString());
+});
