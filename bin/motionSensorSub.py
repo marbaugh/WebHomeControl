589a48441493721d600454a50cb1c4f77f3f6fe5
marbaugh <marbaugh@gmail.com>
2014-04-08 10:39:02 -0400

    Fixed Indent

--- bin/motionSensorSub.py
+++ bin/motionSensorSub.py
@@ -8,14 +8,14 @@ context = zmq.Context()
 socket = context.socket(zmq.SUB)
 
 def sub_motion_sensor_status():
-print "Collecting updates from weather server..."
-socket.connect ("tcp://localhost:%s" % port)
-topicfilter = "motion"
-while True:
-socket.setsockopt(zmq.SUBSCRIBE, topicfilter)
-string = socket.recv()
-topic, messagedata = string.split()
-print topic, messagedata
+	print "Collecting updates from weather server..."
+	socket.connect ("tcp://localhost:%s" % port)
+	topicfilter = "motion"
+	while True:
+		socket.setsockopt(zmq.SUBSCRIBE, topicfilter)
+		string = socket.recv()
+		topic, messagedata = string.split()
+		print topic, messagedata
 
 if __name__ == "__main__":
 	sub_motion_sensor_status()
