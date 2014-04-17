31406e04ace2761fb604480b53ce83dca0fc5e6c
marbaugh <marbaugh@gmail.com>
2014-04-17 01:21:39 +0000

    Troubleshot doorSensotr routes.  Added routes for motionSensor.  Added table inheritance for the hardware row.

--- routes.py
+++ routes.py
@@ -13,25 +13,35 @@ class Status(db.Model):
 	# creating columns for various fields
     __tablename__ = 'status'
     id = db.Column(db.Integer, primary_key = True)
+    hardware=db.Column(db.String, index = True, unique = False)
     time=db.Column(db.DateTime, index = True, unique = False)
     status=db.Column(db.String, index = True, unique = False)
     
+    __mapper_args__ = {
+            'polymorphic_on':hardware,
+            'polymorphic_identity':'none'
+    }
+
     def __init__(self, time, status):
         # Initializes the fields with entered data
         self.time = datetime.now()
         self.status = status
 
 class DoorStatus(Status):
-    pass
+    __mapper_args__ = {
+            'polymorphic_identity':'door'
+    }
 
 class MotionStatus(Status):
-    pass
+    __mapper_args__ = {
+            'polymorphic_identity':'motionSensor'
+    }
 
 class MotorStatus(Status):
     duration = db.Column(db.Integer, index = False, unique = False)
 
     def __init__(self, time, status, duration):
-        super(MotorStatus, self).__init__(time, status)
+        super(MotorStatus, self).__init__(hardware, time, status)
         self.duration = duration
 
 # The default route for the app
@@ -40,7 +50,7 @@ def home():
     return render_template('home.html')
 
 @app.route('/doorSensor/status/<status>', methods = ['POST'])
-def updateStatus(status):
+def doorStatus(status):
     if request.method == 'POST':
         insert_door = None
         if status == 'opened':
@@ -51,30 +61,19 @@ def updateStatus(status):
             pass
     db.session.add(insert_door)
     db.session.commit()
-    flash('Door Status was successfully inserted')
+    return 'success' 
 
-# This view method responds to the URL /motionStatus
-@app.route('/motionStatus')
-def motion():
-    insert_motion = MotionStatus(datetime.now(), 'I See Motion')
+@app.route('/motionSensor/status/<status>', methods = ['POST'])
+def motionStatus(status):
+    if request.method == 'POST':
+        insert_motion = None
+        if status == 'motion':
+            insert_motion = MotionStatus(datetime.now(), status)
+        else:
+            pass
     db.session.add(insert_motion)
     db.session.commit()
-    flash('Comment was successfully submitted')
-
-    get_motion = MotionStatus.query.all()
-    for i in get_motion:
-        print i.time
-    return render_template('motionStatus.html')
-
-# This view method responds to the URL /doorStatus
-@app.route('/doorStatus')
-def door():
-    return render_template('doorStatus.html')
-
-# This view method responds to the URL /motorStatus
-@app.route('/motorStatus')
-def motor():
-    return render_template('motorStatus.html')
+    return 'success' 
 
 if __name__ == '__main__':
   app.run(host='192.168.3.107', debug=True)
